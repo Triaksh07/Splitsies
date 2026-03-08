6a49c23ad3886ca4d92f3f1ead2dd37469828ac6
Triaksh Mitra <triaksh.mitra119@nmims.edu.in>
2026-03-08 22:25:26 +0530

    register ExpenseComment model in __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,9 +3,10 @@ from app.models.group import Group, GroupMember
 from app.models.participant import Participant
 from app.models.expense import Expense, ExpenseSplit, GuestCollection
 from app.models.settlement import Settlement, ExchangeRate
+from app.models.comment import ExpenseComment
 
 __all__ = [
     "User", "Group", "GroupMember", "Participant",
     "Expense", "ExpenseSplit", "GuestCollection",
-    "Settlement", "ExchangeRate",
+    "Settlement", "ExchangeRate", "ExpenseComment",
 ]
